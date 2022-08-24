d0602d81b7040c3eb583347fee706b6544961475
MARS <Sergei-6@bk.ru>
2022-08-24 09:06:59 +0300

    feat: add myth desk

--- js/algorithms.js
+++ js/algorithms.js
@@ -1,4 +1,7 @@
 import ancients from '../assets/Ancients/ancients.js';
+import mythDeck from './mythDeck.js';
+
+console.log(mythDeck);
 
 let ancientsClass = document.querySelector('.ancients');
 let complexityClass = document.querySelector('.complexity');
@@ -30,8 +33,17 @@ let selectCardValue = '';
 function classToggle(selectCard) {
     nameCardList.forEach((el, index) => {
         if (el == selectCard) {
+            let mixResultClass = document.querySelector('.mix-result');
+            let mixUpButton = document.querySelector('.mix-up__button');
+            if (mixResultClass) {
+                mixResultClass.remove();
+            }
+            if (mixUpButton) {
+                mixUpButton.remove();
+            }
             propCardList[index].firstChild.classList.add('active-card');
             selectCardValue = el;
+            addRadioButton();
         } else {
             propCardList[index].firstChild.classList.remove('active-card');
         }
@@ -59,34 +71,47 @@ container.addEventListener('click', (event) => {
     }
 });
 
-
-complexityClass.innerHTML = `
+function addRadioButton() {
+    complexityClass.innerHTML = `
 <div class="col-4"><input id="easy" class="easy" name="complexity" type="radio" value="easy"><label for="easy" class="radio-style"> Легкая</label></div>
 <div class="col-4"><input id="medium" class="medium" name="complexity" type="radio" value="medium"><label for="medium" class="radio-style"> Среднаяя</label></div>
 <div class="col-4"><input id="hard" class="hard" name="complexity" type="radio" value="hard"><label for="hard" class="radio-style"> Тяжелая</label></div>`;
+}
 
+let complexity = '';
 complexityClass.addEventListener('click', (event) => {
-    if (selectCardValue != '' && event.target.value != undefined) {
+    complexity = event.target.value;
+    if (selectCardValue != '' && complexity != undefined) {
         mixUp.innerHTML = `<div class="col-md-6 offset-md-3 mix-up__button"><span class="mix-button">Замешать колоду</span></div>`;
 
-        // console.log('Вы выбрали: ' + selectCardValue);
-        // console.log(event.target.value);
+        console.log('Вы выбрали: ' + selectCardValue);
+        console.log('Сложность: ' + complexity);
     }
 });
 
 mixUp.addEventListener('click', (event) => {
     if (event.target.className == 'mix-button') {
-        mixUp.innerHTML = `
-        <div class="row justify-content-center">
-        <div class="col-3">
-            Колода Мифов
-        </div>
-        <div class="col-3">
-            Выбор карты
-        </div>
-        <div class="col-3">
-            Полученая карта
-      </div>
-      </div>`;
+        if (selectCardValue != '' && complexity != '') {
+            mixUp.innerHTML = `
+            <div class="row justify-content-center mix-result">
+            <div class="col-3">
+                Схема колоды мифов
+            </div>
+            <div class="col-3">
+                Выбор карты
+            </div>
+            <div class="col-3">
+                Полученная карта
+          </div>
+          </div>`;
+        } else {
+            mixUp.innerHTML = `
+            <div class="row justify-content-center">
+                <div class="col-6">
+                    Ошибка выбора!
+                </div>
+            </div>`;
+        }
+
     }
 });
